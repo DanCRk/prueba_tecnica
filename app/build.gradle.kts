5e61e7959cc66460075b4d238ac3392b0c5397d1
DannAv <dancrkyt@gmail.com>
2024-10-02 22:12:36 -0600

    Agregar las dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,21 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
-    namespace = "com.ryutec.pruebatecnica"
+    namespace = "com.dann.pruebatecnica"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.ryutec.pruebatecnica"
+        applicationId = "com.dann.pruebatecnica"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -33,6 +36,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
+    buildToolsVersion = "35.0.0"
 }
 
 dependencies {
@@ -45,4 +55,28 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // nvigation component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+
+
+    // MVVM
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
+    implementation("androidx.activity:activity-ktx:1.2.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
+
+    //Dagger hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+
+    //splashscreen
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,21 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.3")
+        classpath(libs.kotlin.gradle.plugin)
+        // el classpath para el plugin de Hilt compatible
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
